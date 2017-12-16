b64ee7bf3cbc7e17b07317af15710953d8f05598
Ted <tedweiss@users.noreply.github.com>
2017-12-16 00:27:03 -0500

    function - makeCardDeck

--- javascript/pokerHands.js
+++ javascript/pokerHands.js
@@ -1,3 +1,11 @@
-jsFile = () => {
-  return true
+makeCardDeck = () => {
+  let deck = []
+  let suits = ['C', 'D', 'H', 'S']
+  let values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+  suits.forEach(suit => {
+    values.forEach(value => {
+      deck.push(value + suit)
+    })
+  })
+  return deck
 }
