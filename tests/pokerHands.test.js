2010757901ea0e3e1d958e24833ea75425d2aea2
tedweiss <tedweiss@users.noreply.github.com>
2017-12-16 21:55:02 -0500

    test - tell a card's value

--- tests/pokerHands.test.js
+++ tests/pokerHands.test.js
@@ -3,6 +3,9 @@ describe('Handling cards', function () {
     expect(makeCardDeck()[0]).toBe('2C')
   })
   it("should tell a card's suit", function () {
-    expect(Card('2C')).toBe('C')
+    expect(Card('2C').suit).toBe('C')
+  })
+  it("should tell a card's value", function () {
+    expect(Card('2C').value).toBe(2)
   })
 })
