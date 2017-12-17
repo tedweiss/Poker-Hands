62e7fe6ee5d89c5c29ed95b71083756125871d0e
Ted <tedweiss@users.noreply.github.com>
2017-12-16 21:34:20 -0500

    test - tell a card's suit

--- tests/pokerHands.test.js
+++ tests/pokerHands.test.js
@@ -2,4 +2,7 @@ describe('Handling cards', function () {
   it('should make a deck of cards', function () {
     expect(makeCardDeck()[0]).toBe('2C')
   })
+  it("should tell a card's suit", function () {
+    expect(Card('2C')).toBe('C')
+  })
 })
